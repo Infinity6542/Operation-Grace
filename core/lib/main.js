157219897d9049d349764c76def1dea55f975c7e
unknown <isd6542@outlook.com>
2024-08-09 21:12:03 +1000

    Fixed an error which would would incorrectly throw an error if the queue only has one item

--- core/lib/main.js
+++ core/lib/main.js
@@ -124,11 +124,13 @@ class Queue {
     }
     arr.push(arr.shift());
     let v = arr[0];
-    if (a === v) {
-      console.error(
-        "[CRIT] [QUEU-CLASS] [CYCLE] [SHIFT] The queue was not able to be shifted. Trying again..."
-      );
-      arr.push(arr.shift());
+    if (arr.length != 1) {
+      if (a === v) {
+        console.error(
+          "[CRIT] [QUEU-CLASS] [CYCLE] [SHIFT] The queue was not able to be shifted. Trying again..."
+        );
+        arr.push(arr.shift());
+      }
     }
     let temp = x;
     x = y;
