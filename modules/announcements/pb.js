ec947ec8954c599c39cd84ebb40524d53adf2296
unknown <isd6542@outlook.com>
2025-04-14 17:55:09 +1000

    Prep for migration

--- modules/announcements/pb.js
+++ modules/announcements/pb.js
@@ -1,86 +1,86 @@
-const pb = new PocketBase("http://127.0.0.1:8090/");
+// const pb = new PocketBase("http://127.0.0.1:8090/");
 
-if (!pb.authStore.isValid) {
-    localStorage.setItem("login", prompt("Enter your login:"));
-    localStorage.setItem("pass", prompt("Enter your password:"));
-    const login = localStorage.getItem("login");
-    const pass = localStorage.getItem("pass");
-    await pb.admins.authWithPassword(login, pass);
-}
-// To logout, simply call "pb.authStore.clear()"
+// if (!pb.authStore.isValid) {
+//     localStorage.setItem("login", prompt("Enter your login:"));
+//     localStorage.setItem("pass", prompt("Enter your password:"));
+//     const login = localStorage.getItem("login");
+//     const pass = localStorage.getItem("pass");
+//     await pb.admins.authWithPassword(login, pass);
+// }
+// // To logout, simply call "pb.authStore.clear()"
 
 let stack = [];
 
-// Fetch announcements
-const announcementsRaw = await pb.collection("announcements").getFullList({
-  sort: "-created",
-  filter: "validUntil != null && validUntil > @now",
-});
+// // Fetch announcements
+// const announcementsRaw = await pb.collection("announcements").getFullList({
+//   sort: "-created",
+//   filter: "validUntil != null && validUntil > @now",
+// });
 
-// Announcements stats
-let count = Number(announcementsRaw.length);
-let pos = Number(localStorage.getItem("pos"));
+// // Announcements stats
+// let count = Number(announcementsRaw.length);
+// let pos = Number(localStorage.getItem("pos"));
 
-// If not declared, declare
-if (
-  localStorage.getItem("count") == undefined ||
-  localStorage.getItem("pos") == undefined
-) {
-  localStorage.setItem("count", count);
-  localStorage.setItem("pos", 0);
-}
+// // If not declared, declare
+// if (
+//   localStorage.getItem("count") == undefined ||
+//   localStorage.getItem("pos") == undefined
+// ) {
+//   localStorage.setItem("count", count);
+//   localStorage.setItem("pos", 0);
+// }
 
-// Begin the cycle again
-if (pos >= count) {
-  localStorage.setItem("pos", 0);
-  pos = 0;
-}
+// // Begin the cycle again
+// if (pos >= count) {
+//   localStorage.setItem("pos", 0);
+//   pos = 0;
+// }
 
-// Process announcements
-function processAnnouncement(x) {
-  const title = x.title;
-  const msg = x.message;
-  const img =
-    "http://127.0.0.1:8090/api/files/" +
-    x.collectionId +
-    "/" +
-    x.id +
-    "/" +
-    x.img;
-  new ann(title, msg, img);
-}
+// // Process announcements
+// function processAnnouncement(x) {
+//   const title = x.title;
+//   const msg = x.message;
+//   const img =
+//     "http://127.0.0.1:8090/api/files/" +
+//     x.collectionId +
+//     "/" +
+//     x.id +
+//     "/" +
+//     x.img;
+//   new ann(title, msg, img);
+// }
 
-class ann {
-  constructor(title, message, image) {
-    this.title = title;
-    this.message = message;
-    this.image = image;
-    stack.push(this);
-  }
-  destruct() {
-    this.title = null;
-    this.message = null;
-    this.image = null;
-    this.void;
-    this.destruct();
-  }
-}
+// class ann {
+//   constructor(title, message, image) {
+//     this.title = title;
+//     this.message = message;
+//     this.image = image;
+//     stack.push(this);
+//   }
+//   destruct() {
+//     this.title = null;
+//     this.message = null;
+//     this.image = null;
+//     this.void;
+//     this.destruct();
+//   }
+// }
 
-// Subscribe to changes
-pb.collection("announcements").subscribe("*", function (e) {
-  console.log("Change detected. Reloading to apply changes...");
-  location.reload();
-});
+// // Subscribe to changes
+// pb.collection("announcements").subscribe("*", function (e) {
+//   console.log("Change detected. Reloading to apply changes...");
+//   location.reload();
+// });
 
-if (announcementsRaw.length != 0) {
-  processAnnouncement(announcementsRaw[pos]);
+// if (announcementsRaw.length != 0) {
+//   processAnnouncement(announcementsRaw[pos]);
 
-  // Update position in cycle
-  localStorage.setItem("pos", pos + 1);
-  console.log(localStorage.getItem("pos"));
-  pos++;
-}
+//   // Update position in cycle
+//   localStorage.setItem("pos", pos + 1);
+//   console.log(localStorage.getItem("pos"));
+//   pos++;
+// }
 
-console.log(`There is/are ${count} active announcement(s).`);
+// console.log(`There is/are ${count} active announcement(s).`);
 
 export default stack;
